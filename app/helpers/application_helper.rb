a7a5c5104e8f61771383137c5fa48cb2be52b9f3
Charlie Cooksey <cdcooksey@gmail.com>
2014-06-27 22:52:35 -0500

    Got bored and added a table helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def table_helper(header, sub_header, columns, &block)
+    table = ''
+    table << "<div class='panel panel-primary'>"
+    table << "<div class='panel-heading'><strong>#{header}</strong></div>"
+    table << "<div class='panel-body'><p>#{sub_header}</p></div>"
+    table << "<table class='table'><thead><tr>"
+
+    columns.each do |column|
+      table << "<th>#{column}</th>"
+    end
+
+    table << "</tr>"
+    table << "</thead>"
+    table << "<tbody>"
+
+  end
+
+  def end_table
+    table = ''
+    table << "</body>"
+    table << "</table>"
+    table << "</div>"
+    table << "</div>"
+    table << "</div>"
+  end
 end

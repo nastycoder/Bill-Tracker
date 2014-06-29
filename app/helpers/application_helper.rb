ae2adf9033235e9c6f7331ac852f1484b878c022
Charlie Cooksey <cdcooksey@gmail.com>
2014-06-28 22:23:15 -0500

    Use block for table content generation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,28 @@
 module ApplicationHelper
 
-  def table_helper(header, sub_header, columns, &block)
+  def table_helper(header = '', sub_header = '', columns = [], &block)
     table = ''
     table << "<div class='panel panel-primary'>"
     table << "<div class='panel-heading'><strong>#{header}</strong></div>"
-    table << "<div class='panel-body'><p>#{sub_header}</p></div>"
+    table << "<div class='panel-body'><p>#{sub_header}</p></div>" unless sub_header.empty?
     table << "<table class='table'><thead><tr>"
 
     columns.each do |column|
       table << "<th>#{column}</th>"
     end
 
+    table << capture(&block) if block_given?
+
     table << "</tr>"
     table << "</thead>"
     table << "<tbody>"
 
-  end
-
-  def end_table
-    table = ''
     table << "</body>"
     table << "</table>"
     table << "</div>"
     table << "</div>"
     table << "</div>"
+    table
   end
+
 end
